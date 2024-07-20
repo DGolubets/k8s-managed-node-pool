a6e6fde2c3afd0ec82157b86177b77bc078cc1b4
Dmitry Golubets <dgolubets@gmail.com>
2024-07-20 22:36:05 +0100

    Remove manual Docker build script

--- docker/build_and_push.sh
+++
@@ -1,7 +0,0 @@
-PACKAGE="$1"
-NAME="ghcr.io/dgolubets/$PACKAGE"
-VERSION=$(cargo pkgid -p $PACKAGE | cut -d '#' -f 2)
-IMAGE="$NAME:$VERSION"
-
-docker build -t $IMAGE --build-arg PACKAGE=$PACKAGE .
-docker push $IMAGE
